d92047b6c31f621fdd035e57d52a039f02ca53c0
Kirill Kirilenko <kirillkirilenko.tomsk@gmail.com>
2021-12-12 17:35:27 +0700

    untitled.py added to .gitignore

--- wc.py
+++ wc.py
@@ -2,9 +2,12 @@ import os
 import sys
 import argparse
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser('Count lines, words or bytes')
 
 parser.add_argument('-l', help='prints the line count', required=False)
 parser.add_argument('-c', help='prints the byte count', required=False)
 parser.add_argument('-w', help='prints the word count', required=False)
+
+
+
 
