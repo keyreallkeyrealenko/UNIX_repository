26d27070144e31cdd702b6ee7f2162dfafa63388
Kirill Kirilenko <kirillkirilenko.tomsk@gmail.com>
2021-12-14 10:55:56 +0700

    changes in grep.py included to padd linter checks

--- grep.py
+++ grep.py
@@ -6,18 +6,6 @@ import re
 import sys
 
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-
 def dir_path(string):
     # Set 1 when grep takes file instead of stdin
     if os.path.isdir(string):
@@ -42,7 +30,6 @@ def main():
         return 0
     # Handle the case when input is a text file
     if isinstance(args.infile, tuple):
-        found = []
         with open(args.infile[0]) as f:
             text = f.readlines()
         for line in text:
